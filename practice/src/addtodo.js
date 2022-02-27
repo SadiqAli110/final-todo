5aa085aa0520e7058a8e09ab19f5f2b1b57cad49
SadiqAli110 <96784616+SadiqAli110@users.noreply.github.com>
2022-02-27 15:15:18 +0500

    Initial commit

---
+++ practice/src/addtodo.js
@@ -0,0 +1,12 @@
+import React from "react";
+class addTodo extends React.Component {
+            render(){
+                return <div>
+                    {/* <input type="text"
+                    placeholder="Add items..." />
+                    <button>Add Todo</button> */}
+                </div>
+            }
+
+        }
+export default addTodo;
